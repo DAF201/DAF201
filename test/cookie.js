d861a4e3de52cf3e02e6782b34646c357529977b
DAF201 <56328675+DAF201@users.noreply.github.com>
2022-07-22 11:17:10 -0500

    Delete test directory

--- test/cookie.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('keydown', (e) => {
-    if (e.ctrlKey && e.keyCode === 90) {
-        xhr.open("POST", '/register', true);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-        xhr.send();
-    } else {
-        document.getElementById('msg1').innerHTML = e.keyCode
-    }
-})
